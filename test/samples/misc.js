848752825990082a5d63658e676eeb383e5c63f8
flames of love <mechanicofthesequence@gmail.com>
2016-02-25 23:13:52 +0100

    failing test

--- test/samples/misc.js
+++ test/samples/misc.js
@@ -19,3 +19,12 @@ const doubleQuotedString = "this is also \"escaped\"";
 const templateString = `the answer is ${answer}. This is a backtick: \``;
 
 const regex = /you can ignore\/[/]skip me, it's cool/;
+
+function calc (data) {
+  for (i = 0; i < num; i++)
+    something = item[i] / total * 100
+
+  for (i = 0; i < 4; i++) {
+    something = Math.round(totals[i] / total * 100)
+  }
+}
